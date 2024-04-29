b90e7b78434e69dc192f762e6c08eb2b9c226e71
Sanjay <sk5248916@gmail.com>
2024-04-29 11:09:22 +0545

    add create course page

--- CollegeMonitor/Pages/Courses/Courses.cshtml.cs
+++ CollegeMonitor/Pages/Courses/Courses.cshtml.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 public class CoursesModel: PageModel
 {
-    public List<Course> Courses {get; set;}
+     public List<Course> Courses {get; set;}
      public void OnGet()
      {
         CollegeDbContext db = new();
-      Courses = db.Courses.ToList();
+        Courses = db.Courses.ToList();
      }
 }
